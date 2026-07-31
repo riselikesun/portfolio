3a86a731c6468ea9253717716be1e1f4278f31e2
Suraj Sharma <suraj@riselikesun.com>
2026-07-31 08:46:01 +0000

    upgarded version

---
+++ app/components/AppBar.tsx
@@ -0,0 +1,15 @@
+const AppBar = () => {
+    return (
+        <header className="w-full bg-white shadow-md"> 
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-center items-center h-16">
+                    <div className="flex-shrink-0 flex items-center">
+                        <h1 className="text-2xl font-bold text-gray-800 text-center">Rise Like Sun</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default AppBar;
