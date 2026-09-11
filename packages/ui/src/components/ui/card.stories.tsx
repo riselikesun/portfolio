1b8564c9fed1720436663da720ec05b91e4a87ac
Suraj Sharma <surajsharma571994@gmail.com>
2026-09-11 14:11:51 +0000

    fixed test cases

--- packages/ui/src/components/ui/card.stories.tsx
+++ packages/ui/src/components/ui/card.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './card';
 import { Button } from './button';
-import { BlobImage } from './blob-image';
 import Image from 'next/image';
 
 const meta = {
